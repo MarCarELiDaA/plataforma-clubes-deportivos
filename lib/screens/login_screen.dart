bf7bd6b9950ff95eb3b591aed22777c74993717f
MarCarELiDaA <martin.bautista.sanchez@gmail.com>
2026-09-17 16:02:15 +0200

    Corregir logo configurable en Login

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,4 @@
-﻿import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -622,7 +622,7 @@ class _LoginScreenState extends State<LoginScreen> {
                             ],
                           ),
                           child: Image.asset(
-                            club.fondo,
+                            club.logo,
                             fit: BoxFit.contain,
                             errorBuilder: (context, error, stackTrace) {
                               return Icon(
